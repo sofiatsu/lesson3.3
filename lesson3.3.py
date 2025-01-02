2ff7dd5cc2838b58c56c385d10569a6b73c6f8e2
sofiatsu <rfjdghjgh@gmail.com>
2025-01-02 18:27:20 +0100

    2

--- lesson3.3.py
+++ lesson3.3.py
@@ -2,6 +2,6 @@ lst = [1,2,3]
 l_lst = len(lst)
 
 if (l_lst % 2) == 0:
-    print(lst[:(l_lst // 2)], lst[(l_lst // 3):])
+    print(lst[:(l_lst // 2)], lst[(l_lst // 2):])
 else:
     print(lst[:(l_lst // 2 + 1)], lst[(l_lst // 2 + 1):])
