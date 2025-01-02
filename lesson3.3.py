6b31943f3d5e35478d710fb95f5cba2c85555867
sofiatsu <rfjdghjgh@gmail.com>
2025-01-02 18:25:10 +0100

    2

--- lesson3.3.py
+++ lesson3.3.py
@@ -4,4 +4,4 @@ l_lst = len(lst)
 if (l_lst % 2) == 0:
     print(lst[:(l_lst // 2)], lst[(l_lst // 2):])
 else:
-    print(lst[:(l_lst // 2 + 1)], lst[(l_lst // 2 + 1):])
+    print(lst[:(l_lst // 2 + 1)], lst[(l_lst // 2 + 4):])
